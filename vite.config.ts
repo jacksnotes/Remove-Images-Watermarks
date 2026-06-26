23a29fd1d38c636f0c61adb96a98ffa6f9322918
JackNotes <a804955440@gmail.com>
2026-06-26 16:15:26 +0800

    更新代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import eslintPlugin from 'vite-plugin-eslint'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react(), eslintPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        embed: resolve(__dirname, 'embed.html'),
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
